b14ddfa56922436a70264d6626d5af4aa4aa923d
hsuzach <hsuzach@gmail.com>
2022-03-15 17:26:02 -0700

    create inquirer prompts for employees

--- app.js
+++ app.js
@@ -9,10 +9,151 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { resolvePtr } = require("dns");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+let team = [];
+
+
+
+function start(){
+  function setManager(){
+    inquirer.prompt([
+      {
+        name: 'managerName',
+        type: 'input',
+        message: "What is the manager's name?",
+      },
+      {
+        name: 'managerId',
+        type: 'input',
+        message: "What is the manager's employee ID?",
+      },
+      {
+        name: 'managerEmail',
+        type: 'input',
+        message: "What is the manager's email?",
+      },
+      {
+        name: 'managerOfficeNumber',
+        type: 'input',
+        message: "What is the manager's office number?",
+      },
+    ])
+    .then((data) => {
+      
+      const newManager = new Manager(
+        data.managerName, 
+        data.managerId, 
+        data.managerEmail, 
+        data.managerOfficeNumber
+
+      )
+      team.push(newManager);
+      assignTeam();
+      
+    });
+  }
+  
+  function assignTeam(){
+    inquirer.prompt([
+      {
+        name: 'role',
+        type: 'list',
+        message: "What type of employee would you like to add?",
+        choices: ['Engineer', 'Intern', 'I am finished adding members'],
+      }
+    ])
+    .then((data) => {
+      if (data.role === 'Engineer'){
+        inquirer.prompt([
+          {
+            name: 'engineerName',
+            type: 'input',
+            message: "What is this engineer's name?",
+          },
+          {
+            name: 'engineerId',
+            type: 'input',
+            message: "What is the engineer's employee ID?",
+          },
+          {
+            name: 'engineerEmail',
+            type: 'input',
+            message: "What is this engineer's email?",
+          },
+          {
+            name: 'engineerGithub',
+            type: 'input',
+            message: "What is this engineer's GitHub username?",
+          },
+        ])
+        .then((data) =>{
+          const newEngineer = new Engineer(
+            data.engineerName,
+            data.engineerId,
+            data.engineerEmail,
+            data.engineerGithub,
+          );
+          
+          team.push(newEngineer);
+          assignTeam();
+        })
+
+      }
+      if (data.role === 'Intern'){
+        inquirer.prompt([
+          {
+            name: 'internName',
+            type: 'input',
+            message: "What is this intern's name?",
+          },
+          {
+            name: 'internId',
+            type: 'input',
+            message: "What is the intern's Employee ID?",
+          },
+          {
+            name: 'internEmail',
+            type: 'input',
+            message: "What is this intern's email?",
+          },
+          {
+            name: 'internSchool',
+            type: 'input',
+            message: "What school is this intern attending?",
+          },
+        ])
+        .then((data) =>{
+          const newIntern = new Intern(
+            data.internName,
+            data.internId,
+            data.internEmail,
+            data.internSchool,
+          );
+          
+          team.push(newIntern);
+          assignTeam();
+        })
+      }
+      if (data.role === 'I am finished adding members'){
+
+        console.log(team);
+        // renderTeam();
+      }
+      
+    })
+  }
+  setManager();
+
+}
+start();
+  
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
